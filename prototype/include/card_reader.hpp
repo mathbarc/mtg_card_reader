e71e6513f0e786f2cc5688df6103afcd6164f330
Matheus Barcelos de Oliveira <matheus.oliveira@syngenta.com>
2023-12-03 20:18:05 -0300

    adding prototype

---
+++ prototype/include/card_reader.hpp
@@ -0,0 +1,53 @@
+#ifndef CARD_READER_CPP
+#define CARD_READER_CPP
+
+#include <opencv2/core/core.hpp>
+
+enum class Type
+{
+    MONSTER = 0,
+    FIELD = 1,
+    ARTIFACT = 2,   
+    SPELL = 3
+};
+
+enum class ManaColor
+{
+    RED = 0,
+    BLUE = 1,
+    GREEN = 2,
+    BLACK = 3,   
+    WHITE = 4,
+    NOCOLOR = 5
+};
+
+
+struct CardData
+{
+    Type type;
+    ManaColor color;
+    std::string name;
+};
+
+struct CardPosition
+{
+    cv::Point2d topLeft;
+    cv::Point2d topRight;
+    cv::Point2d bottomRight;
+    cv::Point2d bottomLeft;
+};
+
+
+class CardReader
+{
+    public:
+        
+        CardReader();
+        std::vector<CardPosition> findCards(const cv::Mat& image);
+        std::vector<cv::Mat> extractCards(const cv::Mat& image, const std::vector<cv::Rect>& rois);
+        CardData getCardData(const cv::Mat& cardImage);
+
+};
+
+
+#endif
---
+++ prototype/source/card_reader.cpp
@@ -0,0 +1,15 @@
+#include "card_reader.hpp"
+
+
+CardReader::CardReader()
+{}
+
+
+std::vector<CardPosition> CardReader::findCards(const cv::Mat& image)
+{
+    std::vector<CardPosition> cardFound;
+
+
+
+    return cardFound;
+}
